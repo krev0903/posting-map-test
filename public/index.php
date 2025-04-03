e84480b475e39949e5c7f15212ea1b29725086b9
kurebayashi <shotakon77@gmail.com>
2025-04-03 15:59:41 +0900

    setting

--- public/index.php
+++ public/index.php
@@ -4,25 +4,14 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Maintenance Mode
+// Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-// Autoload Composer
+// Register the Composer autoloader...
 require __DIR__.'/../vendor/autoload.php';
 
-// Bootstrap Laravel
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-// Inertia Setup
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
-
-// Send the Response
-$response->send();
-
-// Terminate
-$kernel->terminate($request, $response);
+// Bootstrap Laravel and handle the request...
+(require_once __DIR__.'/../bootstrap/app.php')
+    ->handleRequest(Request::capture());
